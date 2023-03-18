fd935a71348f0d515dae6135d8e66b8a606215df
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-18 23:03:57 +0300

    Update GreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
+     public static void main(String[] args) {
+        GreatestNumberPrinter g = new GreatestNumberPrinter();
+        g.printGreatest(0, 0);
+    }
+    
     public void printGreatest(int first, int second) {
+        if(first > second){
+            System.out.println(first);
+        }else{
+            System.out.println(second);
+        }
     }
 }
